4560b312a61a4c441201783243ac308aa42e030d
Mike Cardillo <mcardillo55@gmail.com>
2016-07-28 01:32:58 -0700

    made password field a PasswordInput on the signup form

--- images/forms.py
+++ images/forms.py
@@ -15,3 +15,6 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput()
+        }
