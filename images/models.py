db52882f0d4a0f14c0246813c732241a0a07b414
Mike Cardillo <mcardillo55@gmail.com>
2016-07-28 03:29:08 -0700

    set created_at only when image is created, not when it is saved

--- images/models.py
+++ images/models.py
@@ -13,6 +13,6 @@ class Image(models.Model):
     title = models.CharField(max_length=100)
     file = models.ImageField(upload_to=content_file_name)
     img_hash = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     uploaded_by = models.ForeignKey('auth.User', blank=True, null=True)
     view_count = models.IntegerField(default=0)
